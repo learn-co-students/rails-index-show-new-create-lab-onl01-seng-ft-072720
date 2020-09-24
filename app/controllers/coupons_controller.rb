1f3ecb7992365e123315ce5546fca6c26a075f69
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-09-24 10:48:29 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,4 +3,9 @@ class CouponsController < ApplicationController
     def index
         @coupons = Coupon.all
     end
+
+    def show
+        # byebug
+        @coupon = Coupon.find_by(id: params[:id])
+    end
 end
