cd9db58c5b9a1933fbc4c66c229e52de1223fb19
HuffDaddy0 <ty.huff24@gmail.com>
2020-09-24 13:11:15 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+
+  end
+
+  def show
+    @coupon = Coupon.find_by(params[:id])
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
+  end
+
 end
